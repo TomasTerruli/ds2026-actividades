7218d362ea3c8642a6a51d91db9d4244dd1ce3ad
Tomas Terruli <tomasterruli7@gmail.com>
2026-04-13 10:46:36 -0300

    Ejercicio 1

---
+++ ejercicios/c03-javascript/ej1-variables/app.js
@@ -0,0 +1,8 @@
+const nombre = 'Tomas';
+const edad = '21';
+const materia = 'Desarrollo de software';
+let contador = 0;
+contador = contador + 1;
+contador = contador + 1;
+contador = contador + 1;
+console.log(`Me llamo ${nombre}, tengo ${edad} años y curso ${materia} y el contador es ${contador}`);
