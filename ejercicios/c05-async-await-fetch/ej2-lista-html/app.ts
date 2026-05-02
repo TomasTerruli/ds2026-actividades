e77cbf64dc42b1b67cdc4ced3ac841e3f2b973d2
Tomas Terruli <tomasterruli7@gmail.com>
2026-05-02 14:29:22 -0300

    Ejercicio 2 — DOM + fetch + agrego carpeta para los archivos del ejercicio 1

---
+++ ejercicios/c05-async-await-fetch/ej2-lista-html/app.ts
@@ -0,0 +1,58 @@
+interface Usuario {
+  id: number;
+  name: string;
+  email: string;
+  phone: string;
+}
+
+async function obtenerUsuarios(): Promise<Usuario[]> {
+  const respuesta = await fetch("https://jsonplaceholder.typicode.com/users");
+
+  if (!respuesta.ok) {
+    throw new Error("Error al obtener los usuarios");
+  }
+
+  const usuarios: Usuario[] = await respuesta.json();
+
+  return usuarios;
+}
+
+// hasta aca es lo mismo que el anterior ejercicio
+
+async function main(): Promise<void>{
+    const parrafoCargando = document.getElementById("cargando");
+    const parrafoError = document.getElementById("error");
+    const listaUsuario = document.getElementById("listaUsuarios");
+
+    try {
+        const usuario = await obtenerUsuarios() //guarda en una const, espera a que responda la funcion obtener uSUARIOS
+
+        if (parrafoCargando !== null){
+            parrafoCargando.style.display = "none"; // hace que si los usuarios se cargan, se oculte el cargando
+        }
+        
+        if (listaUsuario !== null) {
+        for (let i = 0; i < usuario.length; i++) {
+            const item = document.createElement("li"); // aca crea el elemento de la lista
+
+            item.textContent = usuario[i].name + " - " + usuario[i].email; // agrega los elemento correspondientes a la iteracion i a la li
+
+            listaUsuario.appendChild(item); // agrega dentro del ul
+        }
+
+        }
+    }catch(error){ // cuando ocurre un error
+        
+        if (parrafoCargando !== null) {
+            parrafoCargando.style.display="none";
+        }
+
+        if(parrafoError){
+            parrafoError.textContent = "Ocurrio un error al cargar los usuarios";
+            parrafoError.style.color = "red";
+        }
+    }
+}
+// basicamente no muestra nada si esta vacio. Pero si con el try funciona bien tira esa informacion, si va por el lado del error del catch 
+
+main();
