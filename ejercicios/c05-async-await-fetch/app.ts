7d3b0501aa395820382abdc631685f06273005f7
Tomas Terruli <tomasterruli7@gmail.com>
2026-05-02 13:29:11 -0300

    Ejercicio-Fetch-basico-tipado

---
+++ ejercicios/c05-async-await-fetch/app.ts
@@ -0,0 +1,39 @@
+interface Usuario {
+  id: number;
+  name: string;
+  email: string;
+  phone: string;
+}
+// Esta seria la interfaz de usuario con los datos deckarados
+// la interfaz te dice que si o si el objeto usuario tiene que tener estas propiedades
+
+async function obtenerUsuarios(): Promise<Usuario[]> {
+  try {  
+    const respuesta = await fetch("https://jsonplaceholder.typicode.com/users");
+    // Esta seria la interfaz de usuario con los datos deckarados
+    // la interfaz te dice que si o si el objeto usuario tiene que tener estas propiedades
+
+    if (!respuesta.ok) {
+      throw new Error("Error al obtener los usuarios");
+    }
+    // el .ok a la constante respuesta te devuelve un booleano si la peticion hhtp salio bien
+
+    const usuarios: Usuario[] = await respuesta.json(); // te devuelve los datos del cuerpo de la respuesta convertidos a JSON.
+                                                        // ademas le decis a ts q el resultados es un array de usuarios
+    return usuarios; // devolvemos ese array
+  } catch (error) {
+    console.error("Ocurrió un error:", error);   // devuelve si hay error por CONSOLA!!!!
+    return [];
+  }
+}
+
+async function main(): Promise<void> {
+  const usuarios = await obtenerUsuarios();
+  // llamamos a la funcion obtenerUsuarios y esperamos el resultado
+
+    for (let i = 0; i < usuarios.length; i++) {// te recorre el array de usuarios y te tira la info con la sig estructura
+        console.log("Nombre: " + usuarios[i].name + " - Email: " + usuarios[i].email);
+    }   // toda la info del usuario tamb por consola
+}
+
+main();
